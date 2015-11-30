6c56991bc5d0982ef7e0b2ccc406882fcacbf9cf
Mark McConnell <mark@rehabstudio.com>
2015-11-30 16:09:46 +0000

    debug-tool-bar fixed with docker

--- app/core/settings/local.py
+++ app/core/settings/local.py
@@ -10,5 +10,15 @@ MIDDLEWARE_CLASSES += (
     'debug_toolbar.middleware.DebugToolbarMiddleware',
 )
 
-# django-debug-toolbar settings
-INTERNAL_IPS = ('127.0.0.1', '0.0.0.0')
+# Note that django-debug-tool bar does not play nice with docker
+# so we overwrite its default behaviour to always show the toolbar
+# when the locale settings are loaded in, regardless of IP
+DEBUG_TOOLBAR_CONFIG = {
+    'SHOW_TOOLBAR_CALLBACK': 'core.settings.local.show_toolbar',
+}
+
+
+def show_toolbar(request):
+    if request.is_ajax():
+        return False
+    return True
